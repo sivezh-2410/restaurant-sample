ba80ced156cf776351c8dc1067dbe4b392b0d5bd
sivezh-2410 <sivezh.trading@gmail.com>
2026-09-16 16:25:28 +0530

    Update vite.config.ts
    added base to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs.plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/restaurant-sample/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
